62a4a7f4a79197a2c1096d9ab2ea25eedb5ca8d2
mitre88 <bedr10_capacitacion@hotmail.com>
2022-03-17 22:39:05 -0600

    v2

--- src/curso_golang/src/main.go
+++ src/curso_golang/src/main.go
@@ -2,9 +2,24 @@
 package main
 
 import "fmt"   
-
 func main(){
-	fmt.Println("hola mundo desde GO... ")
+// decalaracion de variables
+//declarando constantes---->
+const nombre string=`hola Alejandro Mitre` // una constante no cambia de valor
+const edad int=23
+const peso float32=65.5
+// declaración de variables :
+var nombre_variable string=`hola Alejandro Mitre` // una variable no cambia de valor
+var edad_variable int=23
+var peso_variable float32=65.5
+// presentación en pantalla---------------------->
+fmt.Println(nombre)
+fmt.Println(edad)
+fmt.Println(peso)
+
+fmt.Println(nombre_variable)
+fmt.Println(edad_variable)
+fmt.Println(peso_variable)
 }
 
 
